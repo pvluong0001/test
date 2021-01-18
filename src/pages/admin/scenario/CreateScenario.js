5adcebc3b46601bb57a41b459a619c0cd6ee92a4
luongpv <pvluong0001@gmail.com>
2021-01-18 23:24:36 +0700

    add autosuggest

--- src/pages/admin/scenario/CreateScenario.js
+++ src/pages/admin/scenario/CreateScenario.js
@@ -1,24 +1,83 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 import {Link} from 'react-router-dom';
-import FormGroup from '@components/share/FormGroup';
+import FormGroup from '@components/form/FormGroup';
 import {Form, Formik} from 'formik';
 import * as Yup from 'yup';
-import SubmitBtn from '@components/share/SubmitBtn';
+import SubmitBtn from '@components/form/SubmitBtn';
 import api from '@plugins/api';
+import Autosuggest from 'react-autosuggest';
+import debounce from 'lodash/debounce';
+
+const theme = {
+  container: 'relative',
+  suggestionsContainer: 'absolute bg-white w-full border shadow-lg rounded-b-md'
+}
 
 const CreateScenario = () => {
   const [tags, setTags] = useState([]);
+  const [value, setValue] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
 
-  useEffect(() => {
-    api.get('/tag')
-      .then(res => {
-        setTags(res.data.data)
-      })
-  }, [])
+  const onSuggestionsFetchRequested = useCallback(debounce(({value}) => {
+    api.get(`/tag?s=${value}`)
+    .then(res => {
+      setSuggestions(res.data.data)
+    })
+  }, 300), [])
+
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  }
+
+  const getSuggestionValue = suggestion => suggestion.name;
+
+  const renderSuggestion = suggestion => (
+    <div className="p-2 hover:bg-gray-100 cursor-pointer">
+      {suggestion.name}
+    </div>
+  );
+
+  function onSuggestionSelected(event, { suggestion, suggestionValue, suggestionIndex, sectionIndex, method }) {
+    console.log(suggestionValue, suggestion);
+  }
+
+  function handlePressSearch(e) {
+    // press enter
+    if (e.nativeEvent.keyCode === 13) {
+      createSearch(value)
+      e.preventDefault();
+    }
+  }
+
+  function createSearch(value) {
+    api.post('/tag', {
+      name: value
+    }).then(res => {
+      console.log(res, '~~~');
+    })
+  }
+
+  const onChange = (event, { newValue, method }) => {
+    event.preventDefault();
+    setValue(newValue)
+  }
+
+  const inputProps = {
+    placeholder: "Search Star Wars",
+    value,
+    onChange
+  };
+
+  // useEffect(() => {
+  //   api.get('/tag')
+  //     .then(res => {
+  //       setTags(res.data.data)
+  //     })
+  // }, [])
 
   const Tags = () => {
     return tags.map(tag => (
-      <span className="badge">{tag.name}</span>
+      <span key={tag.id} className="badge">{tag.name}</span>
     ))
   }
 
@@ -56,7 +115,22 @@ const CreateScenario = () => {
               <div className="flex gap-5">
                 <FormGroup name="name" className="flex-1"/>
                 <FormGroup name="tags" className="flex-1">
-                  <input type="text" className="input" placeholder="Search...."/>
+                  <Autosuggest
+                    suggestions={suggestions}
+                    onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+                    onSuggestionsClearRequested={onSuggestionsClearRequested}
+                    getSuggestionValue={getSuggestionValue}
+                    renderSuggestion={renderSuggestion}
+                    inputProps={inputProps}
+                    onSuggestionSelected={onSuggestionSelected}
+                    renderInputComponent={
+                      inputProps => (
+                        <input {...inputProps} type="text" onKeyPress={handlePressSearch} className="input" placeholder="Search...."/>
+                      )
+                    }
+                    theme={theme}
+                  />
+
                   <p className="text-gray-500 text-sm">Type <b>Enter</b> to create a new tag</p>
                   <div className="mt-2">
                     <Tags/>
